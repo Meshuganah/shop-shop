b8ff398b4221d03e99f14743f2d195aa852a2c15
Josh Klein <kleinjjosh74@gmail.com>
2022-07-16 19:53:10 -0400

    added global state variable, and updated state management in components to reflect

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+//Custom methods made for this project
+import { StoreProvider } from './utils/GlobalState';
 import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
@@ -40,15 +42,17 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
+          <StoreProvider>
           <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
